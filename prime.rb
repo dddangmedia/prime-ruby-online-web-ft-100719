59ac5d75b42cf81a7624190781b3e5e8b067b020
Duong Dang <dddangmedia@gmail.com>
2019-10-18 00:19:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
   counter = 2 
   while counter < integer 
-    if integer % counter == 0 
+    if integer % counter == 0 || integer == -1
       return false 
     end
     counter += 1 
